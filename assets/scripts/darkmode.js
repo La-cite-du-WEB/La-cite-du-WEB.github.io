2495c03f286e715457fc87417e0fd0b1076d76c4
EMPTY <testerthe60@gmail.com>
2023-11-26 09:13:03 +0100

    🛠️ Fix Darkmode, responsiveness, add sfx

--- assets/scripts/darkmode.js
+++ assets/scripts/darkmode.js
@@ -1,9 +1,20 @@
+const audio = new Audio("assets/sfx/flashbang.mp3");
+
 const darkmode = () => {
   if (localStorage.getItem('darkmode') === 'true') {
     document.querySelector('body').classList.add('darkmode');
+    document.querySelector('#darkmode-button span').textContent = "Flash Bang";
+    document.querySelector("#darkmode-button iconify-icon").icon = "ph:sun-light";
 
   } else {
     document.querySelector('body').classList.remove('darkmode');
+    document.querySelector('#darkmode-button span').textContent = "Dark Mode";
+    document.querySelector('body').style.display = "none";
+    setTimeout(() => {document.querySelector('body').style.display = "block";}, 200);
+    document.querySelector("#darkmode-button iconify-icon").icon = "ph:moon-light";
+    
+    
+
   }
 };
 
@@ -14,6 +25,7 @@ document.querySelector('#darkmode-button').addEventListener('click', () => {
     localStorage.setItem('darkmode', 'true');
 
   } else {
+    audio.play();
     localStorage.setItem('darkmode', 'false');
   }
 
