151586fbcd51a6a64ebc0a65ff607272979c914b
Zarox28 <kujawskialexian@gmail.com>
2023-12-02 21:26:56 +0100

    - ➕ Add onclick event user gallery - ➕ Add animations

--- assets/scripts/creator.js
+++ assets/scripts/creator.js
@@ -1,2 +1,12 @@
-function expand(img) {
+function showImage(color, img) {
+  document.querySelector('#expanded-image').src = `../../assets/imgs/cats/color/${color}/${img}.webp`;
+  document.querySelector('#expanded').style.display = 'block';
+
+  document.querySelector('#expanded').addEventListener('click', () => {
+    document.querySelector('#expanded').style.animation = 'scale-out-ver-top 0.2s cubic-bezier(0.550, 0.085, 0.680, 0.530) both';
+    setTimeout(() => {
+      document.querySelector('#expanded').style.display = 'none';
+      document.querySelector('#expanded').style.animation = '';
+    }, 500);
+  });
 }
