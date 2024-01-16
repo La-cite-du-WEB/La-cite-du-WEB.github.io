fa31aaf543e7c77d6c9cfaa60ecee348abbdc799
0x454d505459 <testerthe60@gmail.com>
2024-01-16 22:37:09 +0100

    🛠️ Fix darkmode + small transition

--- assets/scripts/darkmode.js
+++ assets/scripts/darkmode.js
@@ -1,30 +1,36 @@
 const audio = new Audio("assets/sfx/flashbang.mp3");
+const themes = ['Dark', 'Light'];
+const texts = ['Flash Bang', 'Dark Mode'];
+const icons = ['ph:sun-light', 'ph:moon-light'];
 
-const darkmode = () => {
-  if (localStorage.getItem('darkmode') === 'true') {
-    document.querySelector('body').classList.add('darkmode');
-    document.querySelector('#darkmode-button span').textContent = "Flash Bang";
-    document.querySelector("#darkmode-button iconify-icon").icon = "ph:sun-light";
+document.getElementById("darkmode-button").addEventListener('click', () => {
+  let actual_theme = document.documentElement.getAttribute("theme-override")
+                    || localStorage.getItem('theme-override')
+                    || 'Dark';
+  let other_theme = +(actual_theme==='Dark');
 
-  } else {
-    document.querySelector('body').classList.remove('darkmode');
-    document.querySelector('#darkmode-button span').textContent = "Dark Mode";
+  document.documentElement.setAttribute('theme-override', themes[other_theme]);
+  document.querySelector('#darkmode-button span').textContent = texts[other_theme];
+  document.querySelector("#darkmode-button iconify-icon").icon = icons[other_theme];
+  localStorage.setItem('theme-override', themes[other_theme]);
+  // if the actual theme is dark that means we switched to light mod
+  if (actual_theme === 'Dark') {
+    audio.play();
     document.querySelector('body').style.display = "none";
     setTimeout(() => {
       document.querySelector('body').style.display = "block";
     }, 200);
-    document.querySelector("#darkmode-button iconify-icon").icon = "ph:moon-light";
   }
-};
 
-darkmode();
-
-document.querySelector('#darkmode-button').addEventListener('click', () => {
-  if (localStorage.getItem('darkmode') === 'false') {
-    localStorage.setItem('darkmode', 'true');
-
-  } else {
-    audio.play();
-    localStorage.setItem('darkmode', 'false');
-  } darkmode();
 });
+
+(() =>{
+  console.log(themes[+(window.matchMedia("(prefers-color-scheme: dark)").matches)])
+  let saved_theme = localStorage.getItem('theme-override')
+                  || themes[+(!window.matchMedia("(prefers-color-scheme: dark)").matches)]
+                  || 'Dark';
+  let theme_index = +(saved_theme === 'Light')
+  document.documentElement.setAttribute('theme-override', saved_theme);
+  document.querySelector('#darkmode-button span').textContent = texts[theme_index];
+  document.querySelector("#darkmode-button iconify-icon").icon = icons[theme_index];
+})()
